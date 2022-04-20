6f4c0a56d2292a15d149343aa3f8334fb5e4e1c0
L3O\ahlstve <vertti.ahlsten@hiq.fi>
2022-04-20 16:14:47 +0300

    Initial commit

---
+++ Frends.Salesforce.CreateSObject/Frends.Salesforce.CreateSObject.Tests/UnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Frends.Salesforce.CreateSObject.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private readonly string _clientSecret = Environment.GetEnvironmentVariable("Salesforce_Client_Secret");
+        private readonly string _password = Environment.GetEnvironmentVariable("Salesforce_Password");
+        private readonly string _securityToken = Environment.GetEnvironmentVariable("Salesforce_Security_Token");
+        private readonly string _clientID = Environment.GetEnvironmentVariable("Salesforce_ClientID");
+
+        private readonly string _domain = @"https://hiqfinlandoy2-dev-ed.my.salesforce.com";
+        private readonly string _username = "testuser@test.fi";
+        private readonly string _authurl = @"https://login.salesforce.com/services/oauth2/token";
+        private readonly CancellationToken _cancellationToken = new();
+
+        private string _userJson;
+
+        private class JsonTest { 
+            private string Name { get; }
+            private DateTime Date { get; }
+
+            public JsonTest(string name)
+            {
+                this.Name = name;
+                this.Date = DateTime.Now;
+            }
+        }
+
+        [SetUp]
+        public void SetUp() {
+            var temp = new JsonTest("Test");
+            _userJson = JsonConvert.SerializeObject(temp);
+        }
+
+        [Test]
+        public async Task CreateSObject()
+        {
+            var input = new Input
+            {
+                Domain = _domain,
+                SObjectAsJson = _userJson
+            };
+
+            var options = new Options
+            {
+                AuthenticationMethod = AuthenticationMethod.AccessToken,
+                AccessToken = await Salesforce.GetAccessToken(_authurl, _clientID, _clientSecret, _username, _password + _securityToken, _cancellationToken)
+            };
+
+            var result = await Salesforce.CreateSObject(input, options, _cancellationToken);
+            Assert.IsTrue(result.RequestIsSuccessful);
+        }
+
+    }
+}
---
+++ Frends.Salesforce.CreateSObject/Frends.Salesforce.CreateSObject/Definitions/Input.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Frends.Salesforce.CreateSObject
+{
+    /// <summary>
+    /// Input-class for ExecuteQuery-task.
+    /// </summary>
+    public class Input
+    {
+        /// <summary>
+        /// Salesforce Domain.
+        /// /services/data/v52.0/query will be added automatically, so the domain is enough.
+        /// </summary>
+        [DefaultValue(@"https://example.my.salesforce.com")]
+        [DisplayFormat(DataFormatString = "Text")]
+        public string Domain { get; set; }
+
+        /// <summary>
+        /// SObject structure as json.
+        /// </summary>
+        [DisplayFormat(DataFormatString = "Text")]
+        public string SObjectAsJson { get; set; }
+    }
+}
