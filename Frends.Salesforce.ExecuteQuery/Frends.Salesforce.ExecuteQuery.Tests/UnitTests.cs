037fc7ee5cbaf64dc77f9e277c3c7b2413f785b8
Janne Pennanen <jjj.pennanen@gmail.com>
2024-08-14 15:24:45 +0300

    Added ClassInitialize to load environment variables from .env, moved salesforce username to environment variable, and added a test method for an invalid query/detailed error response

--- Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery.Tests/UnitTests.cs
+++ Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery.Tests/UnitTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Frends.Salesforce.ExecuteQuery.Definitions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dotenv.net;
+using NUnit.Framework.Internal;
 
 namespace Frends.Salesforce.ExecuteQuery.Tests;
 [TestClass]
@@ -12,11 +15,21 @@ public class UnitTests
     private readonly string _password = Environment.GetEnvironmentVariable("Salesforce_Password");
     private readonly string _securityToken = Environment.GetEnvironmentVariable("Salesforce_Security_Token");
     private readonly string _clientID = Environment.GetEnvironmentVariable("Salesforce_ClientID");
-
-    private readonly string _domain = @"https://hiqfinlandoy2-dev-ed.my.salesforce.com";
-    private readonly string _username = "testuser@test.fi";
+    private readonly string _username = Environment.GetEnvironmentVariable("Salesforce_Username");
+    private readonly string _domain = @"https://frends2-dev-ed.develop.my.salesforce.com";
     private readonly string _authurl = @"https://login.salesforce.com/services/oauth2/token";
     private readonly CancellationToken _cancellationToken = new();
+
+    [ClassInitialize]
+    public static void TestInitialize(TestContext testContext)
+    {
+        // load envs
+        var root = Directory.GetCurrentDirectory();
+        var projDir = Directory.GetParent(root)?.Parent?.Parent?.FullName;
+        DotEnv.Load(
+            options: new DotEnvOptions(
+                envFilePaths: new[] { $"{projDir}{Path.DirectorySeparatorChar}.env.local" }));   
+    }
 
     [TestMethod]
     public async Task ExecuteQuery_QueryWithToken()
@@ -162,5 +175,25 @@ public class UnitTests
         };
 
         await Salesforce.ExecuteQuery(input, options, _cancellationToken);
+    }
+
+    [TestMethod]
+    public async Task InvalidQuery_ReturnsError()
+    {
+        var input = new Input
+        {
+            Domain = _domain,
+            Query = "SELECT NAME from Invalid"
+        };
+
+        var options = new Options
+        {
+            AuthenticationMethod = AuthenticationMethod.AccessToken,
+            AccessToken = await Salesforce.GetAccessToken(_authurl, _clientID, _clientSecret, _username, _password + _securityToken, _cancellationToken)
+        };
+
+        var result = await Salesforce.ExecuteQuery(input, options, _cancellationToken);
+        Assert.IsNotNull(result.ErrorMessage);
+        StringAssert.Contains(result.ErrorMessage, "sObject type 'Invalid' is not supported.");
     }
 }
