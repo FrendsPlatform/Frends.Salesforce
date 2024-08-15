8a9799d36796fd40eb2edcdbbcbaeb65276371ae
Janne Pennanen <jjj.pennanen@gmail.com>
2024-08-15 12:08:38 +0300

    Test update on CreateSObject

--- Frends.Salesforce.CreateSObject/Frends.Salesforce.CreateSObject.Tests/UnitTests.cs
+++ Frends.Salesforce.CreateSObject/Frends.Salesforce.CreateSObject.Tests/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
@@ -7,6 +8,7 @@ using Frends.Salesforce.CreateSObject.Definitions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using RestSharp;
+using dotenv.net;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Frends.Salesforce.CreateSObject.Tests;
@@ -17,9 +19,9 @@ public class UnitTests
     private readonly string _password = Environment.GetEnvironmentVariable("Salesforce_Password");
     private readonly string _securityToken = Environment.GetEnvironmentVariable("Salesforce_Security_Token");
     private readonly string _clientID = Environment.GetEnvironmentVariable("Salesforce_ClientID");
+    private readonly string _username = Environment.GetEnvironmentVariable("Salesforce_Username");
 
-    private readonly string _domain = @"https://hiqfinlandoy2-dev-ed.my.salesforce.com";
-    private readonly string _username = "testuser@test.fi";
+    private readonly string _domain = @"https://frends2-dev-ed.develop.my.salesforce.com";
     private readonly string _authurl = @"https://login.salesforce.com/services/oauth2/token";
 
     private readonly CancellationToken _cancellationToken = new();
@@ -28,6 +30,17 @@ public class UnitTests
     private List<object> _result;
 
     private string _name = "Test" + DateTime.Now.Year + "" + DateTime.Now.Month + "" + DateTime.Now.Day + "" + DateTime.Now.Hour + "" + DateTime.Now.Minute + "" + DateTime.Now.Millisecond;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext testContext)
+    {
+        // load envs
+        var root = Directory.GetCurrentDirectory();
+        var projDir = Directory.GetParent(root)?.Parent?.Parent?.FullName;
+        DotEnv.Load(
+            options: new DotEnvOptions(
+                envFilePaths: new[] { $"{projDir}{Path.DirectorySeparatorChar}.env.local" }));
+    }
 
     [TestInitialize]
     public async Task TestInitialize()
