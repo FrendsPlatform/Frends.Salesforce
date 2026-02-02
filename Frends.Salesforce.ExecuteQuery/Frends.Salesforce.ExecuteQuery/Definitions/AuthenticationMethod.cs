16362c36dd90916b86dad8da81018e1a9508688a
Sebastian Dehman <sebastiandehman@Sebastians-MacBook-Pro.local>
2026-02-02 10:45:09 +0100

    Added new authentication method for Client Credentials flow: https://help.salesforce.com/s/articleView?id=xcloud.remoteaccess_oauth_client_credentials_flow.htm&type=5

--- Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery/Definitions/AuthenticationMethod.cs
+++ Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery/Definitions/AuthenticationMethod.cs
@@ -8,9 +8,13 @@ public enum AuthenticationMethod
     /// <summary>
     /// Authenticate with access token.
     /// </summary>
-    AccessToken,
+    AccessToken = 0,
     /// <summary>
     /// Authenticate by providing required informations to fetch OAuth2 access token.
     /// </summary>
-    OAuth2WithPassword
+    OAuth2WithPassword = 1,
+    /// <summary>
+    /// Authenticate by providing required informations to fetch OAuth2 access token client_credentials.
+    /// </summary>
+    OAuth2WithClientCredentials = 2
 }
