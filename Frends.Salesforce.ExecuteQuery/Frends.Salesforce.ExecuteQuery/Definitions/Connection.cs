d35b4a016c6d750b8d0f8312f72d2e412e69d9fd
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2026-04-01 23:54:38 +0200

    implement for executeQuery

---
+++ Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery/Definitions/Connection.cs
@@ -0,0 +1,107 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Frends.Salesforce.ExecuteQuery.Attributes;
+
+namespace Frends.Salesforce.ExecuteQuery.Definitions;
+
+/// <summary>
+/// Connection parameters.
+/// </summary>
+public class Connection
+{
+    /// <summary>
+    /// Authentication method used to get a Salesforce access token.
+    /// </summary>
+    /// <example>UsernamePasswordOAuth</example>
+    [DefaultValue(AuthenticationMethod.OAuth2WithPassword)]
+    public AuthenticationMethod AuthenticationMethod { get; set; } = AuthenticationMethod.OAuth2WithPassword;
+
+    /// <summary>
+    /// Salesforce instance URL. Required when using the AccessToken authentication method.
+    /// </summary>
+    /// <example>https://mydomain.my.salesforce.com</example>
+    [NotEmptyString]
+    public string InstanceUrl { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Reusable Salesforce access token. Required when using the AccessToken authentication method.
+    /// </summary>
+    /// <example>00Dxx0000000001!AQ8AQExampleToken</example>
+    [PasswordPropertyText]
+    [DefaultValue("")]
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.AccessToken)]
+    [RequiredIf(nameof(AuthenticationMethod), AuthenticationMethod.AccessToken)]
+    public string AccessToken { get; set; } = string.Empty;
+
+    /// <summary>
+    /// OAuth connected app client ID.
+    /// </summary>
+    /// <example>3MVG9d8..ExampleClientId</example>
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword, AuthenticationMethod.OAuth2WithClientCredentials)]
+    [RequiredIf(
+        nameof(AuthenticationMethod),
+        AuthenticationMethod.OAuth2WithPassword,
+        AuthenticationMethod.OAuth2WithClientCredentials)]
+    public string ClientId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// OAuth connected app client secret.
+    /// </summary>
+    /// <example>ExampleClientSecret</example>
+    [PasswordPropertyText]
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword, AuthenticationMethod.OAuth2WithClientCredentials)]
+    [RequiredIf(
+        nameof(AuthenticationMethod),
+        AuthenticationMethod.OAuth2WithPassword,
+        AuthenticationMethod.OAuth2WithClientCredentials)]
+    public string ClientSecret { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Salesforce username.
+    /// </summary>
+    /// <example>integration.user@example.com</example>
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword)]
+    [RequiredIf(nameof(AuthenticationMethod), AuthenticationMethod.OAuth2WithPassword)]
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Salesforce password.
+    /// </summary>
+    /// <example>ExamplePassword</example>
+    [PasswordPropertyText]
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword)]
+    [RequiredIf(nameof(AuthenticationMethod), AuthenticationMethod.OAuth2WithPassword)]
+    public string Password { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Salesforce security token appended to the password for the username-password OAuth flow when required.
+    /// </summary>
+    /// <example>ExampleSecurityToken</example>
+    [PasswordPropertyText]
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword)]
+    [RequiredIf(nameof(AuthenticationMethod), AuthenticationMethod.OAuth2WithPassword)]
+    public string SecurityToken { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The API version to use when making requests to Salesforce.
+    /// If left empty, the default value is v61.0.
+    /// </summary>
+    [DefaultValue("v61.0")]
+    public string ApiVersion { get; set; } = "v61.0";
+
+    /// <summary>
+    /// Also return access token which is fetched during authentication?
+    /// </summary>
+    /// <example>true</example>
+    [DefaultValue(false)]
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword, AuthenticationMethod.OAuth2WithClientCredentials)]
+    public bool ReturnAccessToken { get; set; }
+
+    /// <summary>
+    /// Set whether process will throw an error if targeted id can not be found from Salesforce.
+    /// </summary>
+    /// <example>true</example>
+    [DefaultValue(false)]
+    [UIHint(nameof(AuthenticationMethod), "", AuthenticationMethod.OAuth2WithPassword, AuthenticationMethod.OAuth2WithClientCredentials)]
+    public bool ThrowAnErrorIfNotFound { get; set; }
+}
--- Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery/ExecuteQuery.cs
+++ Frends.Salesforce.ExecuteQuery/Frends.Salesforce.ExecuteQuery/ExecuteQuery.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Frends.Salesforce.ExecuteQuery.Helpers;
 
 [assembly: InternalsVisibleTo("Frends.Salesforce.ExecuteQuery.Tests")]
 
@@ -15,19 +16,19 @@ namespace Frends.Salesforce.ExecuteQuery;
 /// <summary>
 /// Tasks class.
 /// </summary>
-public class Salesforce
+public static class Salesforce
 {
     /// <summary>
     /// Execute a query to Salesforce.
     /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends.Salesforce.ExecuteQuery)
     /// </summary>
     /// <param name="input">Information to update the sobject.</param>
-    /// <param name="options">Information about the salesforce destination.</param>
+    /// <param name="connection">Information about the salesforce destination.</param>
     /// <param name="cancellationToken"></param>
     /// <returns>Object { dynamic Body, bool RequestIsSuccessful, Exception ErrorException, string ErrorMessage, string Token }</returns>
     public static async Task<Result> ExecuteQuery(
         [PropertyTab] Input input,
-        [PropertyTab] Options options,
+        [PropertyTab] Connection connection,
         CancellationToken cancellationToken
     )
     {
@@ -35,70 +36,27 @@ public class Salesforce
 
         try
         {
-            if (string.IsNullOrWhiteSpace(input.Domain)) throw new ArgumentNullException("Domain cannot be empty.");
-            if (string.IsNullOrWhiteSpace(input.Query)) throw new ArgumentNullException("Query cannot be empty.");
+            ValidationHandler.Run(input, connection);
 
             var query = WebUtility.UrlEncode(input.Query);
-            var client = new RestClient($"{input.Domain}/services/data/{input.ApiVersion}/query/?q={query}");
+            var client =
+                new RestClient($"{connection.InstanceUrl}/services/data/{connection.ApiVersion}/query/?q={query}");
             var request = new RestRequest("/", Method.Get);
+            accessToken = await ConnectionHandler.GetAccessToken(connection, cancellationToken);
+            request.AddHeader("Authorization", "Bearer " + accessToken);
 
-            switch (options.AuthenticationMethod)
-            {
-                case AuthenticationMethod.AccessToken:
-                    if (string.IsNullOrWhiteSpace(options.AccessToken))
-                        throw new ArgumentException(
-                            "Access token cannot be null when using Access Token authentication method");
-                    request.AddHeader("Authorization", "Bearer " + options.AccessToken);
-
-                    break;
-                case AuthenticationMethod.OAuth2WithPassword:
-                    accessToken = await GetAccessToken(options.AuthUrl, options.ClientID, options.ClientSecret,
-                        options.Username, options.Password + options.SecurityToken, cancellationToken);
-                    request.AddHeader("Authorization", "Bearer " + accessToken);
-
-                    break;
-            }
-
-            if (!(options.AuthenticationMethod is AuthenticationMethod.OAuth2WithPassword && options.ReturnAccessToken))
+            if (!ConnectionHandler.ShouldReturnToken(connection.AuthenticationMethod, connection.ReturnAccessToken))
                 accessToken = string.Empty;
-
 
             var response = await client.ExecuteAsync(request, cancellationToken);
             dynamic content = JsonConvert.DeserializeObject(response.Content);
-
 
             return new Result(content, response.IsSuccessful, response.ErrorException,
                 response.IsSuccessful ? string.Empty : content[0].Value<string>("message"), accessToken);
         }
         catch (Exception e)
         {
-            return Helpers.ErrorHandler.Handle(e, accessToken);
+            return ErrorHandler.Handle(e, accessToken);
         }
     }
-
-    #region HelperMethods
-
-    /// <summary>
-    /// Get OAuth2 access token.
-    /// This method is public since it is used also in Unit tests.
-    /// </summary>
-    internal static async Task<string> GetAccessToken(string url, string clientId, string clientSecret, string username,
-        string passwordWithSecurityToken, CancellationToken cancellationToken)
-    {
-        var authClient = new RestClient(url);
-        var authRequest = new RestRequest("", Method.Post);
-        authRequest.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-        authRequest.AddParameter("grant_type", "password");
-        authRequest.AddParameter("client_id", clientId);
-        authRequest.AddParameter("client_secret", clientSecret);
-        authRequest.AddParameter("username", username);
-        authRequest.AddParameter("password", passwordWithSecurityToken);
-        var authResponse = await authClient.ExecuteAsync(authRequest, cancellationToken);
-        string accessToken = JsonConvert.DeserializeObject<dynamic>(authResponse.Content).access_token;
-
-        return accessToken;
-    }
-
-    #endregion
-
 }
