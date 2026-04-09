1a34818e169122d88fd1e02db091daf901dd2da5
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2026-04-10 01:58:47 +0200

    dummy test

--- Frends.Salesforce.CreateSObject/Frends.Salesforce.CreateSObject/Helpers/ErrorHandler.cs
+++ Frends.Salesforce.CreateSObject/Frends.Salesforce.CreateSObject/Helpers/ErrorHandler.cs
@@ -8,7 +8,7 @@ internal static class ErrorHandler
     internal static Result Handle(Exception exception, string additionalMessage = "")
     {
         if (string.IsNullOrEmpty(additionalMessage))
-            throw new Exception(exception.Message, exception);
+            throw new Exception("exception.Message", exception);
 
         throw new Exception(additionalMessage, exception);
     }
